1a5919ce4510fb8aef6a49eb189396d955f4713d
Esha-tur-Razia <81893933+Esha-tur-Razia@users.noreply.github.com>
2021-04-24 01:34:17 -0700

    vowel counter added
    Function to count no of vowels in the given string added

--- Task1/src/task1/Task1.java
+++ Task1/src/task1/Task1.java
@@ -31,6 +31,17 @@ public class Task1 {
         }
         System.out.println("No of words in the given sentence are: " + count);
         
+        for (int i = 0; i < string.length(); i++)
+        {
+            char ch = string.charAt(i);
+            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u'
+                    || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U')
+            {
+                count++;
+            }
+        }
+        
+        System.out.println("No of vowelsin the given sentence are: " + count);
         
     }
     
