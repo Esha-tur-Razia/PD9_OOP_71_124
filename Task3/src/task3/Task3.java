57947aabfd59078d9f395abe80ff88a1b76e4fc2
Esha-tur-Razia <81893933+Esha-tur-Razia@users.noreply.github.com>
2021-04-24 11:37:56 -0700

    ridMultipleBlank function added
    Function to remove multiple blanks added

--- Task3/src/task3/Task3.java
+++ Task3/src/task3/Task3.java
@@ -9,13 +9,28 @@ package task3;
  *
  * @author DC
  */
+import java.util.Scanner;
 public class Task3 {
-
+    
+    static String ridMultipleBlanks(String string)
+    {
+        string = string.replaceAll("\\s+", " ");
+        return string;
+    }
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Enter a string:");
+        Scanner obj = new Scanner(System.in);
+        String string = obj.nextLine();
+        
+        String str;
+        str = ridMultipleBlanks(string);
+        System.out.print(str);
+        
     }
     
 }
