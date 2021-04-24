9e98dc2e388f55905418cff1177f1e1a0536a0c1
Esha-tur-Razia <81893933+Esha-tur-Razia@users.noreply.github.com>
2021-04-24 01:13:23 -0700

    First function of word count added
    It counts the no of words in the the given string

--- Task1/src/task1/Task1.java
+++ Task1/src/task1/Task1.java
@@ -9,13 +9,29 @@ package task1;
  *
  * @author DC
  */
+import java.util.Scanner;
 public class Task1 {
-
+      
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        int count;
+        count = 0;
+        System.out.println("Enter a sentence:");
+        Scanner obj = new Scanner(System.in);
+        String string = obj.nextLine();
+        for (int i= 0; i < string.length(); i++)
+        {
+            if (string.charAt(i) == ' ')
+            {
+                count++;
+            }
+        }
+        System.out.println("No of words in the given sentence are: " + count);
+        
+        
     }
     
 }
